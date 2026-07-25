e8a5d6577013b4e1b66815f41594b38955c3412b
cq_wang <cq_wang@trip.com>
2026-07-26 06:29:35 +0800

    初始化仓库

---
+++ double-color-ball/src/main/java/cqwang/java/data/spider/App.java
@@ -0,0 +1,13 @@
+package cqwang.java.data.spider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
