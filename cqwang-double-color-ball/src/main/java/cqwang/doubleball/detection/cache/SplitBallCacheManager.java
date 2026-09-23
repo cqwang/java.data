f3aebfeef1364e76a0abbce2609d22af35acf968
cq_wang <cq_wang@trip.com>
2026-09-23 13:44:13 +0800

    解决编译问题

---
+++ cqwang-double-color-ball/src/main/java/cqwang/doubleball/detection/cache/SplitBallCacheManager.java
@@ -0,0 +1,14 @@
+package cqwang.doubleball.detection.cache;
+
+import cqwang.doubleball.detection.model.data.SplitBall;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SplitBallCacheManager {
+    private static Map<Integer, SplitBall> cache = new HashMap<>();
+
+    private static SplitBall computeIfAbsent(int targetIndex) {
+        return cache.computeIfAbsent(targetIndex, integer -> new SplitBall(targetIndex));
+    }
+}
